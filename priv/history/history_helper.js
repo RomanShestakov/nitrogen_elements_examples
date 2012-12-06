3c29278021be09a4973dcf43f449165cc27e1263
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-12-06 23:41:44 +0000

    add handler for backbutton click

--- priv/history/history_helper.js
+++ priv/history/history_helper.js
@@ -1,27 +1,42 @@
 (function(window,undefined){
 
     // Prepare
-    var History = window.History; // Note: We are using a capital H instead of a lower h
+    var
+    History = window.History; // Note: We are using a capital H instead of a lower h
+    // $ = window.jQuery,
+    // document = window.document;
+    // $window = $(window);
+    // var pushed = false;
+    var timestamps = []; //array of uniq timestamps
+
     if ( !History.enabled ) {
-         // History.js is disabled for this browser.
-         // This is because we can optionally choose to support HTML4 browsers or not.
+        // History.js is disabled for this browser.
+        // This is because we can optionally choose to support HTML4 browsers or not.
         return false;
     }
 
-    // Bind to StateChange Event
-    History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
-        var State = History.getState(); // Note: We are using History.getState() instead of event.state
-        History.log(State.data, State.title, State.url);
-    });
+    // push state
+    function pushState (title, url, anydata) {
+	// creating uniq timestamps
+	var t = new Date().getTime();
+	timestamps[t] = t;
+	// adding to history
+	History.pushState({timestamp:t}, title, url);
+    }
+    // assign to global pushState
+    window.pushState = pushState;
 
-    // // Change our States
-    // History.pushState({state:1}, "State 1", "?state=1"); // logs {state:1}, "State 1", "?state=1"
-    // History.pushState({state:2}, "State 2", "?state=2"); // logs {state:2}, "State 2", "?state=2"
-    // History.replaceState({state:3}, "State 3", "?state=3"); // logs {state:3}, "State 3", "?state=3"
-    // History.pushState(null, null, "?state=4"); // logs {}, '', "?state=4"
-    // History.back(); // logs {state:3}, "State 3", "?state=3"
-    // History.back(); // logs {state:1}, "State 1", "?state=1"
-    // History.back(); // logs {}, "Home Page", "?"
-    // History.go(2); // logs {state:3}, "State 3", "?state=3"
+    // Bind to StateChange Event
+    History.Adapter.bind(window, 'statechange', function(){ // Note: We are using statechange instead of popstate
+        var State = History.getState(); // Note: We are using History.getState() instead of event.state
+	if(State.data.timestamp in timestamps) {
+	    delete timestamps[State.data.timestamp];
+	}
+	else {
+	//page.history_back();
+	    console.log('backbutton fired!');
+        //History.log(State.data, State.title, State.url);
+	}
+    })
 
 })(window);
--- src/tabs.erl
+++ src/tabs.erl
@@ -3,37 +3,42 @@
 -include_lib("nitrogen_elements/include/nitrogen_elements.hrl").
 -compile(export_all).
 
-main() ->
-    #template { file=filename:join([web_common:templates(), "onecolumn.html"])
-}.
+main() -> #template{file=filename:join([web_common:templates(), "onecolumn.html"])}.
 
 title() -> "Tabs Pane Example".
 headline() -> "Tabs Pane Example".
 
-body() -> [
-
+body() ->
+    wf:wire(#api { name=history_back, tag=f1 }),
     %% wf:wire(tabs, #tab_select{tab = 1}),
-    #tabs{
- 	id = tabs,
-        tag = tabs1,
- 	options=[
- 	    {selected, 0},
- 	    %% {event, mouseover},
-	    {cache, true}
- 	],
- 	tabs=[
- 	    #tab{title="Tab 1", url = "/content/tabs2.htm"},
- 	    #tab{title="Tab 2", body=["Tab two body..."]},
-	    #tab{title="Tab 3", body=["Tab three body..."]}
-   	]
-    }
+    [
+	#tabs{
+	    id = tabs,
+	    tag = tabs1,
+	    options=[
+		{selected, 0}
+		%% {event, mouseover},
+		%% {cache, true}
+	    ],
+	    tabs=[
+		#tab{title="Tab 1", url = "/content/tabs2.htm"},
+		#tab{title="Tab 2", body=["Tab two body..."]},
+		#tab{title="Tab 3", body=["Tab three body..."]}
+	    ]
+	}
 ].
 
 tabs_event('tabsselect', TabsTag, TabAnchor, TabPanel, TabIndex) ->
-    ?PRINT({tabs_event, 'tabsselect', TabsTag, TabAnchor, TabPanel, TabIndex});
-tabs_event(EventType, TabsTag, TabAnchor, TabPanel, TabIndex) ->
+    ?PRINT({tabs_event, 'tabsselect', TabsTag, TabAnchor, TabPanel, TabIndex}),
+    pushState(TabIndex);
+tabs_event(_EventType, _TabsTag, _TabAnchor, _TabPanel, _TabIndex) ->
     %% ?PRINT({tabs_event, EventType, TabsTag, TabAnchor, TabPanel, TabIndex}).
     void.
 
-%% pushState() ->
+pushState(TabIndex) ->
+    wf:wire(wf:f("pushState(\"State ~s\", \"?state=~s\", {state:~s});", [TabIndex, TabIndex, TabIndex])).
+    %%ok.
+
+api_event(A, B, C) ->
+    ?PRINT(A), ?PRINT(B), ?PRINT(C).
 
