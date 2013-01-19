e845f2250e2a1723c16657ac2d675b5bed8063ad
Roman Shestakov <romanshestakov@yahoo.co.uk>
2013-01-19 11:36:21 +0000

    add init version for menu

--- src/index.erl
+++ src/index.erl
@@ -21,6 +21,7 @@ body() -> [
 		body = [
 		    #listitem{body = #link{url = "action_dialog_example", body = "Action Dialog"}},
 		    #listitem{body = #link{url = "tabs", body = "Tabs"}},
+		    #listitem{body = #link{url = "menu", body = "Menu"}},
 		    #listitem{body = #link{url = "grid", body = "Grid"}},
 		    #listitem{body = #link{url = "progressbar", body = "Progressbar"}}
 		]
---
+++ src/menu.erl
@@ -0,0 +1,26 @@
+% Nitrogen Elements Examples
+% Copyright (c) 2013 Roman Shestakov (romanshestakov@yahoo.co.uk)
+% See MIT-LICENSE for licensing information.
+
+-module(menu).
+
+-include_lib("nitrogen_elements/include/nitrogen_elements.hrl").
+-compile(export_all).
+
+main() -> #template{file=filename:join([web_common:templates(), "onecolumn.html"])}.
+
+title() -> "Menu Example".
+headline() -> "Menu Example".
+
+body() ->
+    [
+	#menu{
+	    id = menu,
+	    options = [],
+	    items = [
+		#item{title = "USA", items = [#item{title = "New York City"}, #item{title = "Boston"}]},
+		#item{title = "UK"},
+		#item{title = "Russia"}
+	    ]
+	}
+].
