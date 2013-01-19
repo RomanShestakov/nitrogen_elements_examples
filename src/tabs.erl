372987dc16ccdd94a11590a22f66fa53439adb01
Roman Shestakov <romanshestakov@yahoo.co.uk>
2013-01-19 21:52:19 +0000

    fix tabs example

--- src/tabs.erl
+++ src/tabs.erl
@@ -3,21 +3,19 @@
 % See MIT-LICENSE for licensing information.
 
 -module(tabs).
-
 -include_lib("nitrogen_elements/include/nitrogen_elements.hrl").
 -compile(export_all).
--define(EVENT_TABSSHOW, 'tabsshow').
 
 main() -> #template{file=filename:join([web_common:templates(), "onecolumn.html"])}.
-
 title() -> "Tabs Pane Example".
 headline() -> "Tabs Pane Example".
 
 body() ->
     %% bind to tabs 'tabsshow' event
-    wf:wire(tabs, #tab_event_on{event = ?EVENT_TABSSHOW}),
+    wf:wire(tabs, #tab_event_on{type = ?EVENT_TABSACTIVATE, postback = {tabs, ?EVENT_TABSACTIVATE}}),
+
     %% wire tabs_select to show how to change tab after tabs control was initialized
-    wf:wire(tabs, #tab_select{tab = 1}),
+    %% wf:wire(tabs, #tab_select{tab = 1}),
     wf:wire(#api{name=history_back, tag=f1}),
     [
 	#tabs{
@@ -31,15 +29,17 @@ body() ->
 	}
 ].
 
-tabs_event(?EVENT_TABSSHOW, _Tabs_Id, TabIndex) ->
-    ?PRINT({tabs_event, ?EVENT_TABSSHOW}),
-    wf:wire(wf:f("pushState(\"State ~s\", \"?state=~s\", {tabindex:~s});", [TabIndex, TabIndex, TabIndex])).
+event({ID, ?EVENT_TABSACTIVATE}) ->
+    ?PRINT({tabs_event, ?EVENT_TABSACTIVATE}),
+    wf:wire(wf:f("(function(){var index = jQuery(obj('~s')).tabs(\"option\", \"active\");
+                     pushState(\"State\"+index, \"?state=\"+index, {tabindex:index});})();", [ID])).
 
 api_event(history_back, _B, [[_,{data, Data}]]) ->
-    %% ?PRINT({history_back_event, B, Data}),
+    ?PRINT({history_back_event, _B, Data}),
     TabIndex = proplists:get_value(tabindex, Data),
-    wf:wire(tabs, #tab_event_off{event = ?EVENT_TABSSHOW}),
+    wf:wire(tabs, #tab_event_off{event = ?EVENT_TABSACTIVATE}),
     wf:wire(tabs, #tab_select{tab = TabIndex}),
-    wf:wire(tabs, #tab_event_on{event = ?EVENT_TABSSHOW});
+    wf:wire(tabs, #tab_event_on{type = ?EVENT_TABSACTIVATE});
 api_event(A, B, C) ->
     ?PRINT(A), ?PRINT(B), ?PRINT(C).
+
