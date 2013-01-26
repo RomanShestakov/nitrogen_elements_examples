99a9d059d39d3bc97791330dc7097188cb7d48cb
Roman Shestakov <romanshestakov@yahoo.co.uk>
2013-01-26 18:41:26 +0000

    add layout element

--- src/index.erl
+++ src/index.erl
@@ -15,18 +15,39 @@ title() -> "Nitrogen Elements Examples".
 headline() -> "Nitrogen Elements Examples".
 
 body() -> [
-    #panel{
-    	body = [
-    	    #menu{
-		id = elements_menu,
-    		body = [
-    		    #item{url = "action_dialog_example", title = "Action Dialog"},
-    		    #item{url = "tabs", title = "Tabs"},
-    		    #item{url = "menu", title = "Menu"},
-    		    #item{url = "grid", title = "Grid"},
-    		    #item{url = "progressbar", title = "Progressbar"}
-    		]
-    	    }
-    	]
+
+    % Main layout...
+    #layout {
+	%% class="ui-layout-container",
+
+	north=#panel{text = "North"},
+	north_options = [{size, 60}, {spacing_open, 0}, {spacing_closed, 0}],
+
+	west=#panel{text = "West"},
+	west_options=[{size, 140}, {spacing_open, 0}, {spacing_closed, 0}],
+
+	center=#panel{text = "Center"},
+
+	east=#panel{text = "East"},
+	east_options=[{size, 200}, {spacing_open, 0}, {spacing_closed, 0}],
+
+	south=#panel{text = "South"},
+	south_options=[{size, 200}, {spacing_open, 0}, {spacing_closed, 0}]
     }
+
+    %%,
+    %% #panel{
+    %% 	body = [
+    %% 	    #menu{
+    %% 		id = elements_menu,
+    %% 		body = [
+    %% 		    #item{url = "action_dialog_example", title = "Action Dialog"},
+    %% 		    #item{url = "tabs", title = "Tabs"},
+    %% 		    #item{url = "menu", title = "Menu"},
+    %% 		    #item{url = "grid", title = "Grid"},
+    %% 		    #item{url = "progressbar", title = "Progressbar"}
+    %% 		]
+    %% 	    }
+    %% 	]
+    %% }
 ].
